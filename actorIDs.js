dd620965543372a5091c3c944b6d7388c2beb498
Jopekv1 <lukas.jobczyk@gmail.com>
2023-11-26 11:30:31 +0100

    Initial commit

---
+++ actorIDs.js
@@ -0,0 +1,31 @@
+function getActorPage(url){
+    var request = new XMLHttpRequest();
+    request.open('GET', url, false);
+    request.send();
+    parser = new DOMParser();
+    return parser.parseFromString(request.responseText,"text/html"); 
+}
+var castList = document.getElementsByClassName("cast_list")[0].children[0].children
+for (let i = 0; i < castList.length; i++) {
+  let photo = castList[i].getElementsByClassName("primary_photo")
+  if(photo.length == 0){
+    continue;
+  }
+  var actorPage = getActorPage(photo[0].children[0].href);
+  var info = actorPage.getElementsByClassName("sc-7f1a92f5-2 fStAXw");
+  if(info.length == 0){
+    continue;
+  }
+  castList[i].getElementsByTagName("td")[1].getElementsByTagName("a")[0].innerText+=info[0].innerText;
+}
+
+var producentList = document.getElementsByClassName("name")
+for (let i = 0; i < producentList.length; i++) {  
+  let producent = producentList[i].children[0]
+  var actorPage = getActorPage(producent.href);
+  var info = actorPage.getElementsByClassName("sc-7f1a92f5-2 fStAXw");
+  if(info.length == 0){
+    continue;
+  }
+  producent.innerText+=info[0].innerText;
+}
