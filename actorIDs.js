66c12431c26ed3c5a9f7280768ee85145fb36013
Jopekv1 <lukas.jobczyk@gmail.com>
2025-03-23 14:41:27 +0100

    Adjust to new layout v2

--- actorIDs.js
+++ actorIDs.js
@@ -5,8 +5,10 @@ function getActorPage(url){
     parser = new DOMParser();
     return parser.parseFromString(request.responseText,"text/html"); 
 }
+
 var castList = document.getElementsByClassName("ipc-link ipc-link--base name-credits--title-text name-credits--title-text-big")
-  console.log("Found " + castList.length + " entries");
+console.log("Found " + castList.length + " entries");
+
 for (let i = 0; i < castList.length; i++) {
   console.log("Start processing " + i);
   
